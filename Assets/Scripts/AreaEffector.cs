a8a9b0d88989c28b60ef9813bb534aab1f0cdca4
Gustavo de Leon <gdeleontobias@gmail.com>
2018-05-02 09:19:31 -0600

    Gustavo De Leon 17085

---
+++ Assets/Scripts/AreaEffector.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AreaEffector : MonoBehaviour {
+    public float time;
+    public bool  estado;
+    public GameObject areaeffector;
+
+	// Use this for initialization
+	void Start () {
+
+        estado = false;
+        time = 0;
+
+        
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        time = time + Time.deltaTime;
+
+        if (time > 5)
+        {
+            areaeffector.SetActive(estado);
+            time = 0;
+            estado = !estado;            
+        }
+        else
+        {
+            gameObject.SetActive(true);
+            
+        }
+
+    }
+
+    
+}
---
+++ Assets/Scripts/Iniciar.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Iniciar : MonoBehaviour {
+
+    // Use this for initialization
+    void startGame()
+    {
+        //Al presionar el boton "Play", se va a cargar la Scene "Principal" y va a iniciar el juego.
+        
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        SceneManager.LoadScene("Dungeon2", LoadSceneMode.Single);
+    }
+
+}
